2902ce7565a7100f641e029f843f3fe1398f4c5a
Allan Petersen <allanpetersen1209@gmail.com>
2026-05-31 13:35:07 +0200

    add league api

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,7 @@
 export function generateInviteCode(): string {
   const words = [
     'WOLF', 'LION', 'EAGLE', 'BULL', 'HAWK',
-    'FOX',  'BEAR', 'TIGER', 'LYNX', 'PUMA'
+    'FOX',  'BEAR', 'TIGER', 'DOG', 'PUMA'
   ]
   const word = words[Math.floor(Math.random() * words.length)]
   const suffix = Math.random().toString(36).substring(2, 6).toUpperCase()
